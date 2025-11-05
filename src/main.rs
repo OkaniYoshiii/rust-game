11287edb3f3f9b69be5f7a8912cdb078481a73d2
OkaniYoshiii <dragut31620@gmail.com>
2025-11-05 12:04:33 +0100

    feat: configuration du jeu

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,71 @@
-//! The simplest possible example that does something.
-#![allow(clippy::unnecessary_wraps)]
+use std::time::Duration;
 
-use ggez::{
-    event,
-    glam::*,
-    graphics::{self, Color},
-    Context, GameResult,
-};
+use ggez::{self, Context, ContextBuilder, GameResult, conf::{Conf, WindowMode, WindowSetup}, event, graphics::{Canvas, Color, DrawParam, Text}};
 
-struct MainState {
-    pos_x: f32,
-    circle: graphics::Mesh,
+const TARGET_FPS: u32 = 30;
+
+/// # Game State
+/// 
+/// Represent the game state
+/// 
+/// Stores all data related to represent the game state such as
+/// player position, scores, cards etc ...
+struct State {
+    delta_time: Duration,
 }
 
-impl MainState {
-    fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            vec2(0., 0.),
-            100.0,
-            2.0,
-            Color::WHITE,
-        )?;
+impl ggez::event::EventHandler for State {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        while !ctx.time.check_update_time(TARGET_FPS) { return Ok(()); }
 
-        Ok(MainState { pos_x: 0.0, circle })
-    }
-}
-
-impl event::EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
+        self.delta_time = ctx.time.delta();
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas =
-            graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
+    fn draw(&mut self, ctx: &mut Context) -> Result<(), ggez::GameError> {
+        while !ctx.time.check_update_time(TARGET_FPS) { return Ok(()); }
 
-        canvas.draw(&self.circle, Vec2::new(self.pos_x, 380.0));
+        let mut canvas = Canvas::from_frame(ctx, Color::WHITE);
+        let text = Text::new("Rust game");
+        let params = DrawParam{
+            color: Color::BLACK,
+            ..Default::default()
+        };
 
+        canvas.draw(&text, params);
         canvas.finish(ctx)?;
 
+        println!("Delta time : {}ns", self.delta_time.as_nanos());
         Ok(())
     }
 }
+fn main() {
+    let window_setup = WindowSetup{
+        title: "Rust Game".to_owned(),
+        vsync: true,
+        ..Default::default()
+    };
 
-pub fn main() -> GameResult {
-    let cb = ggez::ContextBuilder::new("super_simple", "ggez");
-    let (mut ctx, event_loop) = cb.build()?;
-    let state = MainState::new(&mut ctx)?;
-    event::run(ctx, event_loop, state)
+    let window_mode = WindowMode {
+        width: 960.0,
+        height: 460.0,
+        ..Default::default()
+    };
+
+    let state = State{
+        delta_time: Duration::new(0, 0),
+    };
+
+    let conf = Conf {
+        window_setup: window_setup,
+        window_mode: window_mode,
+        ..Default::default()
+    };
+
+    let (ctx, event_loop) = ContextBuilder::new("game", "OkaniYoshiii")
+        .default_conf(conf)
+        .build()
+        .unwrap();
+
+    event::run(ctx, event_loop, state);
 }
